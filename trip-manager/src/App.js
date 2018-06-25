409e91109ba49ce0a387881687168420b3be422f
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-06-25 11:57:08 +0700

    add redux setup and its reducer

--- trip-manager/src/App.js
+++ trip-manager/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {init as firebaseInit} from './javascripts/firebase';
+import {Provider} from 'react-redux'
+import configureStore from './configureStore'
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,18 +9,21 @@ class App extends Component {
   constructor(props){
     super(props);
     firebaseInit();
+    this.store = configureStore()
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Provider store={this.store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Provider>
     );
   }
 }
