baa389fc50fb0053e38518fd1567de302bc85d59
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-06-26 09:37:48 +0700

    re order directory

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {init as firebaseInit, addNewTrip} from './javascripts/firebase';
 import {Provider} from 'react-redux'
-import configureStore from './components/configureStore';
-import logo from './logo.svg';
+import {init as firebaseInit} from '../../javascripts/firebase';
+import configureStore from '../configureStore';
+import logo from '../../logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -12,14 +12,10 @@ class App extends Component {
     this.store = configureStore();
   }
 
-  add = () => {
-    addNewTrip(3, "new mock 3 trip");
-  }
   render() {
     return (
       <Provider store={this.store}>
         <div className="App">
-          {this.add()}
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
