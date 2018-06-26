2dc549ee79ecd93e4986c568cad3f32ad00b4579
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-06-26 12:04:59 +0700

    fix function not being invoke

--- src/javascripts/auth.js
+++ src/javascripts/auth.js
@@ -3,18 +3,18 @@ import { getGoogleAuthProvider, getFireBaseAuthObject } from './firebase';
 // Sign up with Gmail account
 const signInWithGmailAcc = () => {
     const provider = getGoogleAuthProvider();
-
-    getFireBaseAuthObject.signInWithPopup(provider).then(function (result) {
+    console.log(getFireBaseAuthObject());
+    getFireBaseAuthObject().signInWithPopup(provider).then(function (result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
+        const token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user;
+        const user = result.user;
     }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        var email = error.email;
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
         // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
+        const credential = error.credential;
     });
 };
 
