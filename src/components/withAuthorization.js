903db8be8553cea0f9eeb24ac03b701ae0d2b50a
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-07-01 11:21:35 +0700

    consume context api at withAuthorization HOC

--- src/components/withAuthorization.js
+++ src/components/withAuthorization.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import * as routes from '../constant/routes';
 import { getFireBaseAuthObject } from '../javascripts/firebase';
+import AuthUserContext from './AuthUserContext';
 
-// still need more logic the prevent component from 
-// rendering before verification step in componentDidMount
 const withAuthorization = (authCondition) => (Component) => {
   class WithAuthorization extends React.Component {
     componentDidMount() {
       getFireBaseAuthObject().onAuthStateChanged(authUser => {
-        console.log(authUser);
         if (!authCondition(authUser)) {
           this.props.history.push(routes.SIGN_IN);
         }
       });
     }
     render() {
-      return <Component />;
+      return (
+        // since redirect is handle in ComponentDidMount
+        // we need folowing Context to prevent the page to render before getting redirected
+        <AuthUserContext.Consumer>
+          {authUser => authUser ? <Component /> : null}       
+        </AuthUserContext.Consumer>
+      );
     }
   }
   return withRouter(WithAuthorization);
