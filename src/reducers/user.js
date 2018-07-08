15f79efbefb193ef2c5365838e88b4f3c8434bc9
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-07-08 14:09:13 +0700

    userReducer to update state

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,22 @@
 import _ from 'lodash';
+import userModel from '../javascripts/models/userModel';
 
 let initialState = {
-    trips: [],
+    authUser: null,
 };
 
+const addUser = (state, authUser) => {
+    return {
+        ...state,
+        authUser: userModel(authUser.uid, authUser.name),
+    };
+}
+
 export default (state = initialState, action) => {
-    let newState = _.merge({}, state);
+    // let newState = _.merge({}, state);
     switch (action.type) {
+        case('ADD_AUTH_USER'):
+            return addUser(state, action.authUser);
         default:
             return state;
     }
