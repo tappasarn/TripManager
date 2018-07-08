2e43e2eccfedf572cecd29f8803699a91e1df6d4
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-07-08 14:06:34 +0700

    add config for redux extension

--- src/components/configureStore.js
+++ src/components/configureStore.js
@@ -1,9 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers/index';
 
 export default () => {
-  let middlewares = [thunk]
-  let store = createStore(reducers, applyMiddleware(...middlewares))
+  const middlewares = [thunk];
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(...middlewares)
+  ));
   return store
 }
