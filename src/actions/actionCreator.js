d39b6aafa09cae82f569a77dae8a720e9149def6
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-07-08 20:30:47 +0700

    add logic to also add new user to realtime database while updating redux store

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -1,6 +1,15 @@
-const addAuthUser = authUser => ({
-    type: 'ADD_AUTH_USER',
-    authUser,
-});
+import { addNewUser } from '../javascripts/firebase';
+
+// action creator start to have too much logic
+// TODO: keep action creator clean by moving thunk logic some where else
+const addAuthUser = authUser => {
+    return dispatch => {
+         dispatch({
+            type: 'ADD_AUTH_USER',
+            authUser,
+        });
+        addNewUser(authUser.uid, authUser.name);
+    };
+}
 
 export { addAuthUser };
