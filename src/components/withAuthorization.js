e855cb32054deda4e17b50abd7d714bde9ce270f
Tappasarn Aramboonpong <taramboonpong@Tappasarns-MacBook-Pro.local>
2018-07-08 14:07:09 +0700

    update withAuthorization HOC to read auth Object from redux

--- src/components/withAuthorization.js
+++ src/components/withAuthorization.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import * as routes from '../constant/routes';
 import { getFireBaseAuthObject } from '../javascripts/firebase';
+import { connect } from 'react-redux';
 import AuthUserContext from './AuthUserContext';
 
 const withAuthorization = (authCondition) => (Component) => {
   class WithAuthorization extends React.Component {
     componentDidMount() {
+      console.log('with authorization mounted');
       getFireBaseAuthObject().onAuthStateChanged(authUser => {
-        if (!authCondition(authUser)) {
+        console.log('props', this.props);
+        console.log('auth func', !authCondition(authUser));
+        console.log('auth props', !this.props.authUser);
+        if (!authCondition(authUser) || !this.props.authUser) {
           this.props.history.push(routes.SIGN_IN);
         }
       });
@@ -17,13 +22,20 @@ const withAuthorization = (authCondition) => (Component) => {
       return (
         // since redirect is handle in ComponentDidMount
         // we need folowing Context to prevent the page to render before getting redirected
-        <AuthUserContext.Consumer>
-          {authUser => authUser ? <Component /> : null}       
-        </AuthUserContext.Consumer>
+        <React.Fragment>
+          {this.props.authUser ? <Component /> : null}       
+        </React.Fragment>
       );
     }
   }
-  return withRouter(WithAuthorization);
+  const mapStateToProps = (state) => {
+    return {
+      authUser: state.user.authUser,
+    };
+  };
+  const withAuthorizationAndRouter = withRouter(WithAuthorization);
+  const withAuthorizationAndRouterAndRedux = connect(mapStateToProps)(withAuthorizationAndRouter);
+  return withAuthorizationAndRouterAndRedux;
 }
 
 export { withAuthorization };
