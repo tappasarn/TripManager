ad6d7bb764013681792c6858636ad9f49f392c95
tappasarn <time723@gmail.com>
2018-07-29 19:40:47 +0700

    add paper element for create page

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import { withAuthorization } from '../withAuthorization';
 import { withNavigator } from '../withNavigator';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
-const Create = () => {
-  return <div>This is create page</div>
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    textAlign: 'left',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: '60em',
+    height: '40em',
+    marginTop: '-18em', /*set to a negative number 1/2 of your height*/
+    marginLeft: '-30em', /*set to a negative number 1/2 of your width*/
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+      height: '92vh',
+      marginTop: 0,
+      marginLeft: 0,
+      position: 'relative',
+      top: 0,
+      left: 0,
+    },
+    backgroundColor: theme.palette.background.paper,
+    overflow: 'auto',
+  }),
+});
+
+const Create = (props) => {
+  const { classes } = props;
+  console.log(props);
+  return (
+    <div className='Create'>
+      <Paper className={classes.root} elevation={4}>
+      </Paper>
+    </div>
+  );
 }
 
 Create.displayName = 'Create';
 
 const authCondition = (authUser) => !!authUser;
-const createWithAuthorization = withAuthorization(authCondition)(Create);
+const CreateWithStyle = withStyles(styles)(Create);
+const createWithAuthorization = withAuthorization(authCondition)(CreateWithStyle);
 const createWithAuthorizationAndNavigator = withNavigator('Create')(createWithAuthorization);
+
 export { createWithAuthorizationAndNavigator as Create };
